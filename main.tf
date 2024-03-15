304ed78c1f4de4aa5e39fda3046b286486459d8f
Hristomus <terradis@yandex.ru>
2024-03-15 14:48:39 +0300

    added SG

--- main.tf
+++ main.tf
@@ -59,3 +59,30 @@ resource "aws_security_group" "lb_public_access" {
   }
 }
 
+resource "aws_security_group" "ec2_lb_access" {
+  name   = "ec2-lb-access"
+  vpc_id = module.vpc.vpc_id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ec2_lb_access" {
+  security_group_id = aws_security_group.ec2_lb_access.id
+
+  from_port                    = 80
+  ip_protocol                  = "tcp"
+  to_port                      = 80
+  referenced_security_group_id = aws_security_group.lb_public_access.id
+}
+
+resource "aws_vpc_security_group_egress_rule" "ec2_internet_access" {
+  for_each          = toset(["80", "433"])
+  security_group_id = aws_security_group.ec2_lb_access.id
+
+  cidr_ipv4   = "0.0.0.0/0"
+  from_port   = each.value
+  ip_protocol = "tcp"
+  to_port     = each.value
+
+  tags = {
+    Name = "Internet Access to port ${each.value}"
+  }
+}
